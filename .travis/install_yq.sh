d94315eee7a49eb52cde9901ef8a7fb7aec91bb0
sangitanalkar <40229345+sangitanalkar@users.noreply.github.com>
2020-04-07 16:24:21 +0530

    Update install_yq.sh

--- .travis/install_yq.sh
+++ .travis/install_yq.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_s390x > yq && chmod +x yq
+curl -L https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_$(uname -m) > yq && chmod +x yq
 sudo cp yq /usr/bin/
