e09743a4fe53bdeab8549ac58efd3052a4d41c1d
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-18 13:40:36 -0800

    Fix yq install

--- .travis/install_yq.sh
+++ .travis/install_yq.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl -fsSL -o yq_linux_s390x.tar.gz https://github.com/mikefarah/yq/releases/download/v4.11.1/yq_linux_s390x.tar.gz && tar xf yq_linux_s390x.tar.gz && chmod +x yq_linux_s390x && rm yq_linux_s390x.tar.gz
-sudo mv yq_linux_s390x /usr/bin/
+sudo mv yq_linux_s390x /usr/bin/yq
