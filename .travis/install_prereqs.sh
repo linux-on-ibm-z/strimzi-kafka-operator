4708f7c665aac2913a02c2ccbc4a7e39866f19a6
Sidong-Wei <Sidong.Wei@ibm.com>
2022-01-19 15:32:28 -0500

    Enable docker non-root previlege

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -7,8 +7,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=s390x signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip acl
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-8 40
+sudo service docker start
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock
