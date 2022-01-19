141c0e2e62223cf2a290eafbb8ec2cb916b4165f
Sidong-Wei <Sidong.Wei@ibm.com>
2022-01-19 15:26:22 -0500

    Install Docker engine in prereqs

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -2,8 +2,12 @@
 
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
-sudo apt-get install -y apt-utils
-sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip
+sudo apt-get install -y apt-utils apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=s390x signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
