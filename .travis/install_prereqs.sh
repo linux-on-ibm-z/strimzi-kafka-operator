c0e7bea21b408953499f562e19b03bb44f01f74d
Sidong-Wei <Sidong.Wei@ibm.com>
2022-01-19 15:48:20 -0500

    Remove existing docker package before installing docker engine

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get remove docker docker-engine docker.io containerd runc || true
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 sudo apt-get install -y apt-utils apt-transport-https ca-certificates curl gnupg lsb-release
@@ -7,7 +8,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=s390x signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip acl
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip acl
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
