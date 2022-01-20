0402e470a593eabb596d32e4d6df37ce7326caaf
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-20 05:47:26 -0800

    Debug kaniko

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -8,4 +8,12 @@ sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-8 40
+systemctl daemon-reload
 sudo systemctl restart docker
+
+# test
+cd $HOME
+git clone -b v1.7.0 https://github.com/GoogleContainerTools/kaniko.git
+cd kaniko/
+docker buildx build --platform linux/s390x --load --build-arg GOARCH=s390x -t local/kaniko-project/executor:v1.7.0 -f ./deploy/Dockerfile .
+docker tag local/kaniko-project/executor:v1.7.0 gcr.io/kaniko-project/executor:v1.7.0
