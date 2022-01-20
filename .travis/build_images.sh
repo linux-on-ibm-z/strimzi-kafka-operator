886c0d95b859499f825c4b98b7dd944d61e079e9
Sidong-Wei <Sidong.Wei@ibm.com>
2022-01-20 10:29:45 -0500

    Pull image explicitly for workaround

--- .travis/build_images.sh
+++ .travis/build_images.sh
@@ -3,15 +3,16 @@
 export PATH=$HOME/apache-maven-3.8.2/bin:$PATH
 
 # Install buildx for docker
-# mkdir -p ~/.docker/cli-plugins
-# curl -sSL "https://github.com/docker/buildx/releases/download/v0.6.1/buildx-v0.6.1.linux-s390x" > ~/.docker/cli-plugins/docker-buildx
-# chmod a+x ~/.docker/cli-plugins/docker-buildx
+mkdir -p ~/.docker/cli-plugins
+curl -sSL "https://github.com/docker/buildx/releases/download/v0.6.1/buildx-v0.6.1.linux-s390x" > ~/.docker/cli-plugins/docker-buildx
+chmod a+x ~/.docker/cli-plugins/docker-buildx
 
 # Build kaniko-executor docker image locally
 cd $HOME
 git clone -b v1.7.0 https://github.com/GoogleContainerTools/kaniko.git
 cd kaniko/
-docker buildx create --use
+docker pull golang:1.15 # workaround due to buildx issue
+docker pull debian:buster-slim # workaround due to buildx issue
 docker buildx build --platform linux/s390x --load --build-arg GOARCH=s390x -t local/kaniko-project/executor:v1.7.0 -f ./deploy/Dockerfile .
 docker tag local/kaniko-project/executor:v1.7.0 gcr.io/kaniko-project/executor:v1.7.0
 
