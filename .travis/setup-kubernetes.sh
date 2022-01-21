0021a634248d589f5435e5feae63b34bf2d9b3c6
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-21 07:34:51 -0800

    Fix minikube install - debug 19

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -63,7 +63,7 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
     sudo apt-get install linux-image-$(uname -r) socat -y
     sudo systemctl enable docker.service
     sudo systemctl restart docker
-    minikube start --vm-driver=none --kubernetes-version=v1.23.1 \
+    minikube start --vm-driver=none \
       --extra-config=apiserver.authorization-mode=Node,RBAC \
       --cpus=${MINIKUBE_CPU}
 
