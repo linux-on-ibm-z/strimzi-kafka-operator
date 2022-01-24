d908232c00126db76ba1914ed66837f2d1ec9faa
Sidong-Wei <Sidong.Wei@ibm.com>
2022-01-24 11:10:59 -0500

    Use docker-registry binary

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -9,7 +9,7 @@ echo "deb [arch=s390x signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo apt-get install git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip acl
+sudo apt-get install git make cmake gcc-8 g++-8 tar wget patch ruby conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip acl docker-registry
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
