018d26407905247ff064ea4f2db8640add9eb636
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-25 12:42:46 -0800

    Test Kube

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -69,6 +69,7 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
     sudo systemctl restart docker
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
     sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+    sudo apt-get install -y kubelet kubeadm kubectl
     sudo swapoff -a
     sudo kubeadm reset
     sudo kubeadm init phase certs all
