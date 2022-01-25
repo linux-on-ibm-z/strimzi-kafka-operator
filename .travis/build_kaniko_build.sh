c19252f7d1c00fc554819ddeb77b464c5f2bda6b
Kun-Lu <kun.lu@ibm.com>
2022-01-25 17:49:24 -0500

    Test workspace for sharing tarballs 3

--- .travis/build_kaniko_build.sh
+++ .travis/build_kaniko_build.sh
@@ -8,3 +8,5 @@ echo $HOME
 docker pull gcr.io/kaniko-project/executor:76624697df879f7c3e3348f22b8c986071af4835
 docker tag gcr.io/kaniko-project/executor:76624697df879f7c3e3348f22b8c986071af4835 gcr.io/kaniko-project/executor:v1.8.0
 docker save gcr.io/kaniko-project/executor:v1.8.0 | gzip > kaniko-executor.tar.gz
+docker load < kaniko-executor.tar.gz
+docker run --rm -it gcr.io/kaniko-project/executor:v1.8.0 version
