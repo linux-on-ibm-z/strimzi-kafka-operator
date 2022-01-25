ce738997ccf8796b75372387d4da84428bb6ff43
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-25 12:39:58 -0800

    Test Kube

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -70,9 +70,15 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
     sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
     sudo swapoff -a
-    sudo apt-get install -y kubelet kubeadm kubectl
-    sudo swapoff -a
-    sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+    sudo kubeadm reset
+    sudo kubeadm init phase certs all
+    sudo kubeadm init phase kubeconfig all
+    sudo kubeadm init phase control-plane all --pod-network-cidr=10.244.0.0/16
+    sudo kubeadm init --v=1 --skip-phases=certs,kubeconfig,control-plane --ignore-preflight-errors=all --pod-network-cidr=10.244.0.0/16
+    sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --v=5
+
+    #sudo apt-get install -y kubelet kubeadm kubectl
+    #sudo kubeadm init --pod-network-cidr=10.244.0.0/16
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
