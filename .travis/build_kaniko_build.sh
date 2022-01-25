2295b24a15608e8a63f934d05bdf726c9cbaf860
Kun-Lu <kun.lu@ibm.com>
2022-01-25 17:37:33 -0500

    Test workspace for sharing tarballs 2

--- .travis/build_kaniko_build.sh
+++ .travis/build_kaniko_build.sh
@@ -4,10 +4,7 @@ export PATH=$HOME/apache-maven-3.8.2/bin:$PATH
 
 # Build kaniko-executor docker image locally
 cd $HOME
-git clone -b v1.7.0 https://github.com/GoogleContainerTools/kaniko.git
-cd kaniko/
-docker pull golang:1.15 # workaround due to buildx issue
-docker pull debian:buster-slim # workaround due to buildx issue
-docker buildx build --platform linux/s390x --load --build-arg GOARCH=s390x -t local/kaniko-project/executor:v1.7.0 -f ./deploy/Dockerfile .
-docker tag local/kaniko-project/executor:v1.7.0 gcr.io/kaniko-project/executor:v1.7.0
-docker save gcr.io/kaniko-project/executor:v1.7.0 | gzip > kaniko-executor.tar.gz
+echo $HOME
+docker pull gcr.io/kaniko-project/executor:76624697df879f7c3e3348f22b8c986071af4835
+docker tag gcr.io/kaniko-project/executor:76624697df879f7c3e3348f22b8c986071af4835 gcr.io/kaniko-project/executor:v1.8.0
+docker save gcr.io/kaniko-project/executor:v1.8.0 | gzip > kaniko-executor.tar.gz
--- .travis/build_kaniko_test.sh
+++ .travis/build_kaniko_test.sh
@@ -2,6 +2,8 @@
 
 export PATH=$HOME/apache-maven-3.8.2/bin:$PATH
 
+find . -name '*kaniko-executor.tar.gz*'
+
 docker load < kaniko-executor.tar.gz
 
-docker run --rm -it gcr.io/kaniko-project/executor:v1.7.0 version
+docker run --rm -it gcr.io/kaniko-project/executor:v1.8.0 version
