4534b810b979621abf139508438515892771347b
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-26 09:58:31 -0800

    Test Kube

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -7,8 +7,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends t
 sudo apt-get install -y apt-utils
 sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip apt-transport-https kubelet kubeadm kubectl
 #sudo kubeadm reset
-#sudo kubeadm init phase certs all
-#sudo kubeadm init phase kubeconfig all
+sudo kubeadm init phase certs all
+sudo kubeadm init phase kubeconfig all
 #sudo kubeadm init phase control-plane all --pod-network-cidr=10.244.0.0/16
 #sudo kubeadm init --v=1 --skip-phases=certs,kubeconfig,control-plane --ignore-preflight-errors=all --pod-network-cidr=10.244.0.0/16
 #sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --v=5
