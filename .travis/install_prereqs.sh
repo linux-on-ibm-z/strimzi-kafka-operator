6653226d2be5c35e9dc4340fe09c9e30daa568d7
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-26 09:18:47 -0800

    Test Kube

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -5,8 +5,7 @@ echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 sudo apt-get install -y apt-utils
-sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip apt-transport-https
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip apt-transport-https kubelet kubeadm kubectl
 sudo kubeadm reset
 sudo kubeadm init phase certs all
 sudo kubeadm init phase kubeconfig all
