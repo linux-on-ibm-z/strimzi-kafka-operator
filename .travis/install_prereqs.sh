4f9b104aaf365b12c8eb9081633b52900288a141
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-26 08:40:58 -0800

    Test Kube

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -4,6 +4,16 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 sudo apt-get install -y apt-utils
 sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip
+sudo apt -y install apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo kubeadm reset
+sudo kubeadm init phase certs all
+sudo kubeadm init phase kubeconfig all
+sudo kubeadm init phase control-plane all --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --v=1 --skip-phases=certs,kubeconfig,control-plane --ignore-preflight-errors=all --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --v=5
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
