ef41f9e304c69a0b88b9fcd721fdc513b4faa242
rposts <rishi@grimness1.fyre.ibm.com>
2022-01-26 09:00:59 -0800

    Test Kube

--- .travis/install_prereqs.sh
+++ .travis/install_prereqs.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 sudo apt-get install -y apt-utils
-sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip
-sudo apt -y install apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && sudo apt-get update
+sudo apt-get install -y git make cmake gcc-8 g++-8 tar wget patch ruby curl conntrack shellcheck openssl libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libarchive-dev diffutils gzip file procps python3 perl zip apt-transport-https
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo kubeadm reset
 sudo kubeadm init phase certs all
