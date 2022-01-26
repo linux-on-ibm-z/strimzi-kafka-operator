ec1e12841b2bacd37f441864493558146e6563f7
Kun-Lu <kun.lu@ibm.com>
2022-01-25 21:01:44 -0500

    Test workspace for sharing tarballs 8

--- .travis/build_kaniko_build.sh
+++ .travis/build_kaniko_build.sh
@@ -3,12 +3,10 @@
 export PATH=$HOME/apache-maven-3.8.2/bin:$PATH
 
 echo $(pwd)
+CUR_PATH=$(pwd)
 # Build kaniko-executor docker image locally
-cd $HOME
-echo $HOME
+cd ${HOME}/build
+echo "${HOME}/build"
 docker pull gcr.io/kaniko-project/executor:76624697df879f7c3e3348f22b8c986071af4835
 docker tag gcr.io/kaniko-project/executor:76624697df879f7c3e3348f22b8c986071af4835 gcr.io/kaniko-project/executor:v1.7.0
-docker save --output kaniko-executor.tar gcr.io/kaniko-project/executor:v1.7.0
-docker load --input kaniko-executor.tar
-docker run --rm gcr.io/kaniko-project/executor:v1.7.0 version
-mv kaniko-executor.tar kaniko-executor
+docker save gcr.io/kaniko-project/executor:v1.7.0 | gzip > ${CUR_PATH}/kaniko-executor.tar.gz
--- .travis/build_kaniko_test.sh
+++ .travis/build_kaniko_test.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH=$HOME/apache-maven-3.8.2/bin:$PATH
+find . -name '*kaniko-executor.tar.gz*'
 
-find . -name '*kaniko-executor*'
-
-mv kaniko-executor kaniko-executor.tar
-
-docker load --input kaniko-executor.tar
+docker load < kaniko-executor.tar.gz
 
 docker run --rm gcr.io/kaniko-project/executor:v1.7.0 version
